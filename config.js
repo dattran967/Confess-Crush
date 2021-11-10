b9c0385fa3fced8cc1701481b1843bc5b7eef1f0
dattran967 <94061256+dattran967@users.noreply.github.com>
2021-11-10 21:41:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    titleWeb: "em ơi! yêu anh Đạt không",
+    introTitle: 'các em của anh',
+    introDesc: `trái đất có rất nhiều loại khoai, nhưng em thích nhất là khoai anh Đạt`,
+    btnIntro: '^^kakakakkakakakkakcoonkak^^',
+    title: 'em yêu anh vì khoai anh to hay là do anh đẹp trai 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnYes: 'đẹp trai <33',
+    btnNo: 'khoai to :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'vì anh quá đẹp trai khoai to nên em mới yêu <33333333',
+    mess: 'cám ơn emm 🥰. ngại ngùng quá 😘😘',
+    messDesc: 'Tối nay 2h, mình qua đón đi chơi nha.',
+    btnAccept: 'trời ơi, em dồng ý, ưeuỵỵuydfuksfuksdks',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
