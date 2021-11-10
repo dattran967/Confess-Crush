f51068ce6e36e71c66ef9c732428b34f5bf1ef1a
dattran967 <94061256+dattran967@users.noreply.github.com>
2021-11-10 22:17:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "em yêu ơi. yêu anh k em😊 ",
     introTitle: 'gửi các em yêu của anh',
-    introDesc: `trái đất có rất nhiều loại khoai, nhưng em thích nhất là khoai anh   `,
-    btnIntro: '^^kakakakakto^^',
+    introDesc: `trái đất có rất nhiều loại khoai, nhưng em thích nhất là khoai anh Đ   `,
+    btnIntro: '^^ảo ma canada lazada shopee tiki^^',
     title: 'em yêu anh là do khoai anh to hay là do anh đẹp trai 🥰',
     desc: 'hay là em đạ say khi nhìn thấy body me ',
     btnYes: 'đẹp trai   <33',
     btnNo: ' chắc chắn là khoai to :3',
-    question: 'vì răng em lại thích anh ❤❤❤ t <3',
-    btnReply: ' <3',
+    question: 'cám ơn em nha, mà răng em lại thích anh ❤❤❤ t <3',
+    btnReply: ' hãy trả lời đi nào coo bé',
     reply: 'vì anh đạ đẹp trai mà khoai còn to nựa con Vỹ, con Dương, con Vũ còn thích nói chi em<33333333',
     mess: 'anh biết mà 🥰. Yêu em của anh nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h ở bãi rác cửa lò anh qua đón đi chơi ở hồ tây nha.',
-    btnAccept: 'Okiiiii lun cccdmmm lol má <3',
+    btnAccept: 'Okiiiii lun cccdmmm lol má mày <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
